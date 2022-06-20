fc8b15a69d82a4af7533dd1cdda8d12a6d855c88
Neamoth <neamothku@gmail.com>
2022-06-20 18:55:54 +0600

    feat: Api for slideJs

---
+++ src/api/Sliderapi.js
@@ -0,0 +1,19 @@
+export const ApiSlides = [
+    {
+        src: require("../components/img/slider.png"),
+        active: true,
+        content:{
+            h2: "Welcome To Our Shop",
+            p: "Upto 20% Discount"
+        },
+        backgound: 'bg-[#fff]'
+    },
+    {
+        src: require("../components/img/slider2.png"),
+        content:{
+            h2: "Hot Deal Only For Today",
+            p: "Upto 50% Discount"
+        },
+        backgound: 'bg-[#fff]'
+    },
+]
